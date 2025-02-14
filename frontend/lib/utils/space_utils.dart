2cdec7c449924e063f5ab52ede50d65f17ad0899
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-02-15 00:09:07 +0530

    new spacing added

--- frontend/lib/utils/space_utils.dart
+++ frontend/lib/utils/space_utils.dart
@@ -5,3 +5,15 @@ Widget sizedBoxH8() {
     height: 8,
   );
 }
+
+Widget sizedBoxH10() {
+  return SizedBox(
+    height: 10,
+  );
+}
+
+Widget sizedBoxH20() {
+  return SizedBox(
+    height: 20,
+  );
+}
