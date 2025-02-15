e5b7b61269a5706a96cae7f996ad59e90693b58c
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-02-15 20:02:30 +0530

    fixed the login screen

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -31,17 +31,19 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.only(
-              left: 30,
-              right: 30,
-              bottom: 15,
-              top: 5,
-            ),
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: 30,
+            right: 30,
+            bottom: 10,
+            top: 5,
+          ),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -56,130 +58,143 @@ class _LoginScreenState extends State<LoginScreen> {
                   fontWeight: FontWeight.w400,
                   fontSize: 14,
                 ),
-                sizedBoxH8(),
-                AppTextButton(
-                  buttonText: "Wallet Connect",
-                  onPressed: () async {},
-                ),
-                Divider(),
-                sizedBoxH8(),
-                Form(
-                  key: _formKey,
+                Expanded(
                   child: Column(
+                    mainAxisAlignment:
+                        MainAxisAlignment.center, // Center content
                     children: [
-                      textFormField(
-                        _nameController,
-                        'Username',
-                        (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your name';
-                          }
-                          return null;
-                        },
-                      ),
                       sizedBoxH15(),
-                      textFormField(
-                        _passwordController,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        'Password',
-                        (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter a password';
-                          }
-                          if (value.length < 8) {
-                            return 'Password must be at least 8 characters long';
-                          }
-                          RegExp regex = RegExp(
-                              r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$');
-                          if (!regex.hasMatch(value)) {
-                            return 'Password must contain at least one uppercase letter, one number, and one special character';
-                          }
-                          return null;
-                        },
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _obscureText
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _obscureText = !_obscureText;
-                            });
-                          },
-                        ),
-                        obscureText: _obscureText,
-                      ),
-                    ],
-                  ),
-                ),
-                sizedBoxH8(),
-                Padding(
-                  padding: const EdgeInsets.only(right: 4.0),
-                  child: Align(
-                    alignment: Alignment.topRight,
-                    child: GestureDetector(
-                      onTap: () {
-                        materialRouteNavigator(
-                          context,
-                          ForgetPasswordScreen(),
-                        );
-                      },
-                      child: RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
+                      Form(
+                        key: _formKey,
+                        child: Column(
                           children: [
-                            textSpan(
-                              'Forget Password?',
-                              fontSize: 14,
+                            sizedBoxH15(),
+                            AppTextButton(
+                              buttonText: "Wallet Connect",
+                              onPressed: () async {},
+                            ),
+                            Divider(),
+                            sizedBoxH15(),
+                            textFormField(
+                              _nameController,
+                              'Username',
+                              (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your name';
+                                }
+                                return null;
+                              },
+                            ),
+                            sizedBoxH15(),
+                            textFormField(
+                              _passwordController,
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              'Password',
+                              (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter a password';
+                                }
+                                if (value.length < 8) {
+                                  return 'Password must be at least 8 characters long';
+                                }
+                                RegExp regex = RegExp(
+                                    r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$');
+                                if (!regex.hasMatch(value)) {
+                                  return 'Password must contain at least one uppercase letter, one number, and one special character';
+                                }
+                                return null;
+                              },
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _obscureText
+                                      ? Icons.visibility_off
+                                      : Icons.visibility,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    _obscureText = !_obscureText;
+                                  });
+                                },
+                              ),
+                              obscureText: _obscureText,
                             ),
                           ],
                         ),
                       ),
-                    ),
+                      sizedBoxH8(),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: GestureDetector(
+                          onTap: () {
+                            materialRouteNavigator(
+                              context,
+                              ForgetPasswordScreen(),
+                            );
+                          },
+                          child: RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                              children: [
+                                textSpan(
+                                  'Forget Password?',
+                                  fontSize: 14,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      sizedBoxH8(),
+                      PasswordValidations(hasMinLength: hasMinLength),
+                      sizedBoxH15(),
+                      AppTextButton(
+                        buttonText: "Login",
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            materialRouteNavigatorRep(
+                              context,
+                              LoginScreen(),
+                            );
+                          }
+                        },
+                      ),
+                    ],
                   ),
                 ),
-                sizedBoxH8(),
-                PasswordValidations(hasMinLength: hasMinLength),
-                sizedBoxH15(),
-                AppTextButton(
-                  buttonText: "Login",
-                  onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      materialRouteNavigatorRep(
-                        context,
-                        LoginScreen(),
-                      );
-                    }
-                  },
-                ),
-                sizedBoxH8(),
-                Center(
-                  child: GestureDetector(
-                    onTap: () {
-                      materialRouteNavigator(
-                        context,
-                        SignUpScreen(),
-                      );
-                    },
-                    child: RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        children: [
-                          textSpan(
-                            'Don\'t Have An Account?',
-                            fontSize: 14,
+                // Bottom Section
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          materialRouteNavigator(
+                            context,
+                            SignUpScreen(),
+                          );
+                        },
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            children: [
+                              textSpan(
+                                'Don\'t Have An Account?',
+                                fontSize: 14,
+                              ),
+                              textSpan(
+                                ' Create Account',
+                                fontSize: 14,
+                                color: const Color.fromRGBO(36, 124, 255, 1),
+                              ),
+                            ],
                           ),
-                          textSpan(
-                            ' Create Account',
-                            fontSize: 14,
-                          ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
+                    Divider(),
+                  ],
                 ),
-                Divider(),
               ],
             ),
           ),
