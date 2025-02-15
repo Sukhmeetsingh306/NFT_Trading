fb90d57937240fc4783f5f77cbfa07facc007739
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-02-15 15:17:47 +0530

    changed the background color

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white30),
         useMaterial3: true,
       ),
       home: SignUpScreen(),
