7de61107c4a72875cd2b651c41de91e6d9a0cdac
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-02-15 20:06:26 +0530

    color changed

--- frontend/lib/screens/signup_screen.dart
+++ frontend/lib/screens/signup_screen.dart
@@ -424,6 +424,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                           textSpan(
                             ' Login',
                             fontSize: 14,
+                            color: const Color.fromRGBO(36, 124, 255, 1),
                           ),
                         ],
                       ),
