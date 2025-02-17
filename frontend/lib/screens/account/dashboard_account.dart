e4d62824544ae2c106293fe2407874beb4304ec0
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-02-18 00:58:43 +0530

    dashboard code. almost completed

--- frontend/lib/screens/account/dashboard_account.dart
+++ frontend/lib/screens/account/dashboard_account.dart
@@ -64,165 +64,255 @@ class DashboardAccount extends StatelessWidget {
           ],
         ),
       ),
-      body: SafeArea(
-        child: Background(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 18.0, top: 18),
-                child: Row(
-                  children: [
-                    const CircleAvatar(
-                      radius: 40,
-                      // backgroundImage: AssetImage(
-                      //   'assets/images/t.png',
-                      // ),
-                    ),
-                    const SizedBox(width: 20),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text("Username",
+      body: Background(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 18.0, top: 18),
+                  child: Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 40,
+                        // backgroundImage: AssetImage(
+                        //   'assets/images/t.png',
+                        // ),
+                      ),
+                      const SizedBox(width: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text("Username",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black)),
+                          SizedBox(height: 5),
+                          Text(
+                            "UID: 123456",
                             style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black)),
-                        SizedBox(height: 5),
-                        Text(
-                          "UID: 123456",
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.black87,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Spacer(),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.calendar_today,
-                          size: 20, color: Colors.black),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 35),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    dashBoardOutlineButton('Level 1  >'),
-                    const SizedBox(width: 15),
-                    dashBoardOutlineButton('20 Points  >'),
-                  ],
-                ),
-              ),
-              sizedBoxH10(),
-              containerWhite(
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    googleText(
-                      'Wallet Balance',
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    sizedBoxH8(),
-                    income(
-                      '100.00',
-                      fontWeight: FontWeight.bold,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15.0),
-                      child: Divider(),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          SizedBox(width: width * 0.38),
-                          googleText(
-                            'Daily Income',
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          SizedBox(width: width * 0.03),
-                          googleText(
-                            'Total Income',
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
+                              fontSize: 14,
+                              color: Colors.black87,
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                    earning(context, 'Comprehensive', '100', '100'),
-                    earning(context, 'Reserve', '0', '90000'),
-                    earning(context, 'Team', '50000', '0'),
-                    earning(context, 'Activity', '0', '0'),
-                    earning(context, 'Stake', '0', '0'),
-                  ],
+                      Spacer(),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.calendar_today,
+                            size: 20, color: Colors.black),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              sizedBoxH8(),
-              containerWhite(
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    googleText(
-                      'Wallet Balance',
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15.0),
-                      child: Divider(),
-                    ),
-                    Column(
-                      children: [
-                        Row(
+                Padding(
+                  padding: const EdgeInsets.only(left: 35),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      dashBoardOutlineButton('Level 1  >'),
+                      const SizedBox(width: 15),
+                      dashBoardOutlineButton('20 Points  >'),
+                    ],
+                  ),
+                ),
+                sizedBoxH10(),
+                containerWhite(
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      googleText(
+                        'Wallet Balance',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      sizedBoxH8(),
+                      income(
+                        '100.00',
+                        fontWeight: FontWeight.bold,
+                      ),
+                      Divider(),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
-                            teamColumn('0', 'Community\n rewards'),
-                            teamColumn('0', 'Valid\n Members'),
-                            teamColumn('0', 'A enthusiast\n  '),
-                            teamColumn('0', 'B+C\n enthusiasts'),
+                            SizedBox(width: width * 0.38),
+                            googleText(
+                              'Daily Income',
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            SizedBox(width: width * 0.03),
+                            googleText(
+                              'Total Income',
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ],
                         ),
-                        sizedBoxH15(),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            teamColumnImage(
-                              'assets/icons/community.png',
-                              'Community\nenthusiasts',
-                            ),
-                            teamColumnImage(
-                              'assets/icons/trophy.png',
-                              'Community\ncontributions',
-                            ),
-                            teamColumnImage(
-                              'assets/icons/orders.png',
-                              'Community\norders',
-                            ),
-                            teamColumnImage(
-                              'assets/icons/share.png',
-                              'Referral\n ',
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      Divider(),
+                      earning(context, 'Comprehensive', '100', '100'),
+                      earning(context, 'Reserve', '0', '90000'),
+                      earning(context, 'Team', '50000', '0'),
+                      earning(context, 'Activity', '0', '0'),
+                      earning(context, 'Stake', '0', '0'),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                sizedBoxH8(),
+                containerWhite(
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      googleText(
+                        'My Teams',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      Divider(),
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              teamColumn('0', 'Community\n rewards'),
+                              teamColumn('0', 'Valid\n Members'),
+                              teamColumn('0', 'A enthusiast\n  '),
+                              teamColumn('0', 'B+C\n enthusiasts'),
+                            ],
+                          ),
+                          sizedBoxH15(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              teamColumnImage(
+                                'assets/icons/community.png',
+                                'Community\nenthusiasts',
+                              ),
+                              teamColumnImage(
+                                'assets/icons/trophy.png',
+                                'Community\ncontribution',
+                              ),
+                              teamColumnImage(
+                                'assets/icons/orders.png',
+                                'Community\norders',
+                              ),
+                              teamColumnImage(
+                                'assets/icons/share.png',
+                                'Referral\n ',
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                sizedBoxH8(),
+                containerWhite(
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      googleText(
+                        'My Orders',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      Divider(),
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              teamColumn('0', 'Orders'),
+                              teamColumn('0', 'Processing'),
+                              teamColumn('0', 'Bought'),
+                              teamColumn('0', 'Sold'),
+                            ],
+                          ),
+                          sizedBoxH15(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              teamColumnImage(
+                                'assets/icons/community.png',
+                                'NFTs               ',
+                              ),
+                              teamColumnImage(
+                                'assets/icons/trophy.png',
+                                'Details               ',
+                              ),
+                              teamColumnImage(
+                                'assets/icons/orders.png',
+                                'Deposit         ',
+                              ),
+                              teamColumnImage(
+                                'assets/icons/share.png',
+                                'Withdraw',
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                sizedBoxH8(),
+                containerWhite(
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      googleText(
+                        'Common Functions',
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      Divider(),
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              teamColumnImage(
+                                'assets/icons/community.png',
+                                'Tutorials',
+                              ),
+                              teamColumnImage(
+                                'assets/icons/trophy.png',
+                                'Settings',
+                              ),
+                              teamColumnImage(
+                                'assets/icons/orders.png',
+                                'Mint',
+                              ),
+                              teamColumnImage(
+                                'assets/icons/share.png',
+                                'Collections',
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -231,28 +321,30 @@ class DashboardAccount extends StatelessWidget {
 }
 
 Widget teamColumn(String amount, String name) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      teamText(amount, fontWeight: FontWeight.bold),
-      SizedBox(width: 8),
-      teamText(name),
-    ],
+  return SizedBox(
+    width: 80, // Ensures all columns have equal width
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        teamText(amount),
+        teamText(name),
+      ],
+    ),
   );
 }
 
 Widget teamColumnImage(String image, String name) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      ImageIcon(
-        AssetImage(image),
-        size: 26,
-      ),
-      const SizedBox(height: 8),
-      teamText(name),
-    ],
+  return SizedBox(
+    width: 80, // Ensures equal width
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset(image, width: 24, height: 24),
+        const SizedBox(height: 8),
+        teamText(name),
+      ],
+    ),
   );
 }
