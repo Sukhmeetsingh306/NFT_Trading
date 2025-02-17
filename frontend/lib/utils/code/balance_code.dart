a4fb69a99546498f49d0326c5ec1e5f4b60d504b
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-02-17 19:16:35 +0530

    fixed code spacing issue

--- frontend/lib/utils/code/balance_code.dart
+++ frontend/lib/utils/code/balance_code.dart
@@ -11,14 +11,14 @@ Widget income(
     children: [
       Image.asset(
         'assets/images/t.png',
-        width: 17,
-        height: 17,
+        width: 15,
+        height: 15,
       ),
       const SizedBox(width: 6),
       googleText(
         text,
         // walletBalance.toStringAsFixed(2),
-        fontSize: fontSize ?? 24,
+        fontSize: fontSize ?? 20,
         fontWeight: fontWeight ?? FontWeight.normal,
       ),
     ],
@@ -32,40 +32,52 @@ Widget earning(
   double? fontSize,
   FontWeight? fontWeight,
 }) {
-  return Row(
-    children: [
-      googleText(
-        text,
-        // walletBalance.toStringAsFixed(2),
-        fontSize: fontSize ?? 24,
-        fontWeight: fontWeight ?? FontWeight.normal,
-      ),
-      const SizedBox(width: 6),
-      Image.asset(
-        'assets/images/t.png',
-        width: 17,
-        height: 17,
-      ),
-      const SizedBox(width: 6),
-      googleText(
-        dailyIncome,
-        // walletBalance.toStringAsFixed(2),
-        fontSize: fontSize ?? 24,
-        fontWeight: fontWeight ?? FontWeight.normal,
-      ),
-      const SizedBox(width: 6),
-      Image.asset(
-        'assets/images/t.png',
-        width: 17,
-        height: 17,
-      ),
-      const SizedBox(width: 6),
-      googleText(
-        totalIncome,
-        // walletBalance.toStringAsFixed(2),
-        fontSize: fontSize ?? 24,
-        fontWeight: fontWeight ?? FontWeight.normal,
-      ),
-    ],
+  return Padding(
+    padding: const EdgeInsets.only(right: 8.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        googleText(
+          text,
+          // walletBalance.toStringAsFixed(2),
+          fontSize: fontSize ?? 14,
+          fontWeight: fontWeight ?? FontWeight.normal,
+        ),
+        const SizedBox(width: 6),
+        Row(
+          children: [
+            Image.asset(
+              'assets/images/t.png',
+              width: 14,
+              height: 14,
+            ),
+            const SizedBox(width: 4),
+            googleText(
+              dailyIncome,
+              // walletBalance.toStringAsFixed(2),
+              fontSize: fontSize ?? 14,
+              fontWeight: fontWeight ?? FontWeight.normal,
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            const SizedBox(width: 4),
+            Image.asset(
+              'assets/images/t.png',
+              width: 14,
+              height: 14,
+            ),
+            const SizedBox(width: 4),
+            googleText(
+              totalIncome,
+              // walletBalance.toStringAsFixed(2),
+              fontSize: fontSize ?? 14,
+              fontWeight: fontWeight ?? FontWeight.normal,
+            ),
+          ],
+        ),
+      ],
+    ),
   );
 }
