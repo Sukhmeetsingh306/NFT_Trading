83264930bf177803ea44596c448333ee729bfddf
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-03-15 02:25:16 +0530

    image added

--- flow/lib/screen/login_screen.dart
+++ flow/lib/screen/login_screen.dart
@@ -9,10 +9,51 @@ class LoginScreen extends StatelessWidget {
       body: Stack(
         children: [
           Container(
+            width: MediaQuery.sizeOf(context).width,
+            height: MediaQuery.sizeOf(context).height,
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/images/backgroundImage.png'),
+                image: AssetImage('assets/images/png/backgroundImage.png'),
                 fit: BoxFit.cover,
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 40, 0, 0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(24, 24, 0, 20),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          'assets/images/png/im.png',
+                          width: 70,
+                          height: 80,
+                          fit: BoxFit.fitWidth,
+                        ),
+                        /*
+                          SvgPicture.asset(
+                          'assets/images/svg/files.svg',
+                          width: 170,
+                          height: 60,
+                          fit: BoxFit.fitWidth,
+                        ),
+                           */
+                        // if (!(Theme.of(context).brightness == Brightness.dark))
+                        //   Image.asset(
+                        //     'assets/images/finWallet_logo_landscapeDark@3x.png',
+                        //     width: 170,
+                        //     height: 60,
+                        //     fit: BoxFit.fitWidth,
+                        //   ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
