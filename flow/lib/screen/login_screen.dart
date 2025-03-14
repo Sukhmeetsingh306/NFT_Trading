79cdecacb8fa6e1449685128dc1c1e85f8f7cbf0
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-03-15 02:05:38 +0530

    added the background

--- flow/lib/screen/login_screen.dart
+++ flow/lib/screen/login_screen.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/backgroundImage.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
