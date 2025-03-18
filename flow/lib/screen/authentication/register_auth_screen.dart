596f8ca7d0fc44a557df20d5f449ca04eb220faf
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-03-18 18:54:12 +0530

    sources to create the registeration page

---
+++ flow/lib/screen/authentication/register_auth_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegisterAuthScreen extends StatelessWidget {
+  const RegisterAuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
