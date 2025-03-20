0710237fc17bc92a0b9aac50056ef5154fb686ee
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-03-20 22:34:59 +0530

    added animation on the row as the wallet

--- flow/lib/screen/main/dashboard_main_screen.dart
+++ flow/lib/screen/main/dashboard_main_screen.dart
@@ -14,44 +14,43 @@ class DashboardMainScreen extends StatefulWidget {
 
 class _DashboardMainScreenState extends State<DashboardMainScreen>
     with TickerProviderStateMixin {
-  // late AnimationController _animationController;
-  // late Animation<double> _fadeAnimation;
-  // late Animation<Offset> _slideAnimation;
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _scaleAnimation;
 
-  // @override
-  // void initState() {
-  //   super.initState();
+  @override
+  void initState() {
+    super.initState();
 
-  //   _animationController = AnimationController(
-  //     vsync: this,
-  //     duration: const Duration(milliseconds: 950),
-  //   );
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 600),
+      vsync: this,
+    );
 
-  //   _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-  //     CurvedAnimation(
-  //       parent: _animationController,
-  //       curve: Curves.easeInOut,
-  //     ),
-  //   );
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
 
-  //   _slideAnimation = Tween<Offset>(
-  //     begin: const Offset(0.0, 0.2),
-  //     end: Offset.zero,
-  //   ).animate(
-  //     CurvedAnimation(
-  //       parent: _animationController,
-  //       curve: Curves.easeOutCubic,
-  //     ),
-  //   );
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0.0, 0.3),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
 
-  //   _animationController.forward();
-  // }
+    _scaleAnimation = Tween<double>(begin: 0.4, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
 
-  // @override
-  // void dispose() {
-  //   _animationController.dispose();
-  //   super.dispose();
-  // }
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,59 +69,71 @@ class _DashboardMainScreenState extends State<DashboardMainScreen>
                 ),
               ],
             ),
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: MediaQuery.sizeOf(context).width * 0.92,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 6,
-                          color: Color(0x4B1A1F24),
-                          offset: Offset(
-                            0.0,
-                            2,
-                          ),
-                        )
-                      ],
-                      gradient: LinearGradient(
-                        colors: [Color(0xFF00968A), Color(0xFFF2A384)],
-                        stops: [0, 1],
-                        begin: AlignmentDirectional(0.94, -1),
-                        end: AlignmentDirectional(-0.94, 1),
-                      ),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Column(
+            FadeTransition(
+              opacity: _fadeAnimation,
+              child: SlideTransition(
+                position: _slideAnimation,
+                child: ScaleTransition(
+                  scale: _scaleAnimation,
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                    child: Row(
                       mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Padding(
-                          padding:
-                              EdgeInsetsDirectional.fromSTEB(20, 20, 20, 40),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              googleInterTextWeight4Font16(
-                                'UID:123445',
-                              ),
+                        Container(
+                          width: MediaQuery.sizeOf(context).width * 0.92,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 6,
+                                color: Color(0x4B1A1F24),
+                                offset: Offset(
+                                  0.0,
+                                  2,
+                                ),
+                              )
                             ],
+                            gradient: LinearGradient(
+                              colors: [Color(0xFF00968A), Color(0xFFF2A384)],
+                              stops: [0, 1],
+                              begin: AlignmentDirectional(0.94, -1),
+                              end: AlignmentDirectional(-0.94, 1),
+                            ),
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(20, 0, 0, 20),
                           child: Column(
                             mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              googleInterTextWeight4Font16('Wallet Balance'),
                               Padding(
-                                padding:
-                                    EdgeInsetsDirectional.fromSTEB(0, 5, 0, 0),
-                                child: googleInterTextWeight4Font16('1029'),
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    20, 20, 20, 40),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    googleInterTextWeight4Font16(
+                                      'UID:123445',
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    20, 0, 0, 20),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    googleInterTextWeight4Font16(
+                                        'Wallet Balance'),
+                                    Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          0, 5, 0, 0),
+                                      child:
+                                          googleInterTextWeight4Font16('1029'),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ],
                           ),
@@ -130,7 +141,7 @@ class _DashboardMainScreenState extends State<DashboardMainScreen>
                       ],
                     ),
                   ),
-                ],
+                ),
               ),
             ),
           ],
