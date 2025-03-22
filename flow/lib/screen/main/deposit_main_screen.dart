8571b70c81dcdb9ad5104d41cfc7404b623bee31
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-03-22 22:21:34 +0530

    created the screen

---
+++ flow/lib/screen/main/deposit_main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DepositMainScreen extends StatefulWidget {
+  const DepositMainScreen({super.key});
+
+  @override
+  State<DepositMainScreen> createState() => _DepositMainScreenState();
+}
+
+class _DepositMainScreenState extends State<DepositMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
