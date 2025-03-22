7a6114602e3ce5628de5957e4c2a02292959b3d3
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-03-23 00:39:07 +0530

    added the card and fields

--- flow/lib/screen/main/deposit_main_screen.dart
+++ flow/lib/screen/main/deposit_main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../utils/fonts/google_fonts_utils.dart';
 import '../../utils/routes/navigation_routes.dart';
@@ -15,6 +16,26 @@ class DepositMainScreen extends StatefulWidget {
 }
 
 class _DepositMainScreenState extends State<DepositMainScreen> {
+  String username = "User";
+  String usdtBalance = '0.00';
+
+  bool isHidden = false;
+
+  Future<void> _loadUsername() async {
+    final pref = await SharedPreferences.getInstance();
+    setState(() {
+      username = pref.getString('username') ?? "User";
+      usdtBalance = pref.getString('usdtBalance') ?? '0.00';
+    });
+    //print("Loaded Username: $username"); // 🔍 Debug SharedPreferences
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUsername();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +45,7 @@ class _DepositMainScreenState extends State<DepositMainScreen> {
         child: Column(
           children: [
             Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(12, 0, 12, 0),
+              padding: EdgeInsetsDirectional.fromSTEB(12, 0, 12, 10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -43,6 +64,105 @@ class _DepositMainScreenState extends State<DepositMainScreen> {
                       Icons.cancel_outlined,
                       size: 32,
                       color: ColorTheme.color.whiteColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(12, 4, 12, 0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.sizeOf(context).width * 0.92,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 6,
+                          color: Color(0x4B1A1F24),
+                          offset: Offset(
+                            0.0,
+                            2,
+                          ),
+                        )
+                      ],
+                      gradient: LinearGradient(
+                        colors: [Color(0xFF00968A), Color(0xFFF2A384)],
+                        stops: [0, 1],
+                        begin: AlignmentDirectional(0.94, -1),
+                        end: AlignmentDirectional(-0.94, 1),
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding:
+                              EdgeInsetsDirectional.fromSTEB(20, 20, 20, 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  googleInterTextWeight4Font16(
+                                    isHidden
+                                        ? "******"
+                                        : username, // Toggle visibility
+                                  ),
+                                  const SizedBox(height: 5),
+                                  googleInterTextWeight4Font16(
+                                    isHidden ? "UID: ******" : "UID: 123456",
+                                  ),
+                                ],
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isHidden = !isHidden; // Toggle state
+                                  });
+                                },
+                                icon: Icon(
+                                  isHidden
+                                      ? Icons.visibility_off
+                                      : Icons.visibility, // Eye icon toggle
+                                  size: 25,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              EdgeInsetsDirectional.fromSTEB(20, 0, 35, 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              googleInterTextWeight4Font16('Wallet Balance'),
+                              googleInterTextWeight4Font16(
+                                isHidden ? '****' : usdtBalance,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              EdgeInsetsDirectional.fromSTEB(20, 0, 35, 20),
+                          child: googleInterTextWeight4Font16(
+                            isHidden
+                                ? '**** **** **** ****'
+                                : '1206 1245 6586 7578',
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
