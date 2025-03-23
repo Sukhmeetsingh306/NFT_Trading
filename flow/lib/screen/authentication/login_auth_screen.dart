c99421ad8727b2b565fb2cc014178b429296007d
Sukhmeetsingh306 <137325973+Sukhmeetsingh306@users.noreply.github.com>
2025-03-23 23:55:15 +0530

    code fixed for the screen size issue

--- flow/lib/screen/authentication/login_auth_screen.dart
+++ flow/lib/screen/authentication/login_auth_screen.dart
@@ -51,6 +51,203 @@ class _LoginAuthScreenState extends State<LoginAuthScreen> {
     );
   }
 
+  Widget pageCode(bool isLargeScreen) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: isLargeScreen ? 500 : double.infinity,
+      ),
+      child: Container(
+        height: isLargeScreen ? MediaQuery.of(context).size.height * 0.8 : null,
+        padding: EdgeInsets.symmetric(
+          horizontal: isLargeScreen ? 50 : 20,
+          vertical: isLargeScreen ? 50 : 20,
+        ),
+        decoration: isLargeScreen
+            ? BoxDecoration(
+                color: Colors.black.withValues(alpha: 0.7),
+                border: Border.all(color: Colors.grey, width: 2),
+                borderRadius: BorderRadius.circular(20), // Rounded edges
+              )
+            : null, // No border on small screens
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: isLargeScreen
+                  ? MainAxisAlignment.start
+                  : MainAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/images/png/im.png',
+                  width: 70,
+                  height: 80,
+                  fit: BoxFit.fitWidth,
+                ),
+                SizedBox(width: 10),
+                googleInterText('Application Title'),
+              ],
+            ),
+            sizedBoxH15(),
+            googleReadexProText('Welcome Back', fontSize: 37),
+            sizedBoxH8(),
+            googleInterText(
+              'Login to access your Account',
+              fontSize: 20,
+            ),
+            sizedBoxH15(),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  textFormField(
+                    _emailController,
+                    "Email Address",
+                    'Enter your email...',
+                    (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      } else if (!RegExp(r"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
+                          .hasMatch(value)) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                    suffixIcon: DropdownButtonHideUnderline(
+                      child: DropdownButton<String>(
+                        value: selectedDomain,
+                        alignment: Alignment.centerRight,
+                        dropdownColor: Colors.black,
+                        onChanged: (String? newValue) {
+                          if (newValue != null) {
+                            setState(() {
+                              selectedDomain = newValue;
+                              _updateEmail();
+                            });
+                          }
+                        },
+                        items: emailDomains.map<DropdownMenuItem<String>>(
+                          (String domain) {
+                            return DropdownMenuItem<String>(
+                              value: domain,
+                              child: Text(
+                                "@$domain",
+                                style: GoogleFonts.getFont(
+                                  'Inter',
+                                  color: ColorTheme.color.textWhiteColor,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            );
+                          },
+                        ).toList(),
+                      ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    autofillHints: [AutofillHints.email],
+                    onChanged: (value) => _updateEmail(),
+                  ),
+                  sizedBoxH15(),
+                  textFormField(
+                    _passwordController,
+                    'Password',
+                    "Enter your password...",
+                    (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      if (value.length < 8) {
+                        return 'Password must be at least 8 characters long';
+                      }
+                      RegExp regex = RegExp(
+                          r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$');
+                      if (!regex.hasMatch(value)) {
+                        return 'Password must contain at least one uppercase letter, one number, and one special character';
+                      }
+                      return null;
+                    },
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscureText ? Icons.visibility_off : Icons.visibility,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    ),
+                    obscureText: _obscureText,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                  ),
+                  sizedBoxH15(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      textButton(
+                        'Forget Password?',
+                        () =>
+                            materialNamedRouteNavigator(context, '/forgetPage'),
+                      ),
+                      elevatedButton(
+                        'Login',
+                        () => materialNamedRouteNavigator(
+                            context, '/dashboardPage'),
+                      ),
+                    ],
+                  ),
+                  sizedBoxH15(),
+                  Opacity(
+                    opacity: 0.8,
+                    child: InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: isLargeScreen ? 400 : double.infinity,
+                        height: 44,
+                        decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.7),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            googleInterText(
+                              'Don\'t have an account?',
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 5),
+                              child: InkWell(
+                                onTap: () => materialNamedRouteNavigator(
+                                    context, '/registerPage'),
+                                child: googleInterText(
+                                  'Create',
+                                  color: ColorTheme.color.buttonBackgroundColor,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_rounded,
+                              color: ColorTheme.color.buttonBackgroundColor,
+                              size: 24,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,206 +265,14 @@ class _LoginAuthScreenState extends State<LoginAuthScreen> {
                     fit: BoxFit.cover,
                   ),
                 ),
-                child: Center(
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxWidth: isLargeScreen ? 500 : double.infinity,
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: isLargeScreen ? 50 : 20,
-                        vertical: 50,
+                child: isLargeScreen
+                    ? Center(child: pageCode(isLargeScreen))
+                    : Padding(
+                        padding: const EdgeInsets.only(
+                          top: 60,
+                        ),
+                        child: pageCode(isLargeScreen),
                       ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: isLargeScreen
-                                ? MainAxisAlignment.start
-                                : MainAxisAlignment.start,
-                            children: [
-                              Image.asset(
-                                'assets/images/png/im.png',
-                                width: 70,
-                                height: 80,
-                                fit: BoxFit.fitWidth,
-                              ),
-                              SizedBox(width: 10),
-                              googleInterText('Application Title'),
-                            ],
-                          ),
-                          sizedBoxH15(),
-                          googleReadexProText('Welcome Back', fontSize: 37),
-                          sizedBoxH8(),
-                          googleInterText(
-                            'Login to access your Account',
-                            fontSize: 20,
-                          ),
-                          sizedBoxH15(),
-                          Form(
-                            key: _formKey,
-                            child: Column(
-                              children: [
-                                textFormField(
-                                  _emailController,
-                                  "Email Address",
-                                  'Enter your email...',
-                                  (value) {
-                                    if (value == null || value.isEmpty) {
-                                      return 'Please enter your email';
-                                    } else if (!RegExp(
-                                            r"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
-                                        .hasMatch(value)) {
-                                      return 'Please enter a valid email';
-                                    }
-                                    return null;
-                                  },
-                                  suffixIcon: DropdownButtonHideUnderline(
-                                    child: DropdownButton<String>(
-                                      value: selectedDomain,
-                                      alignment: Alignment.centerRight,
-                                      dropdownColor: Colors.black,
-                                      onChanged: (String? newValue) {
-                                        if (newValue != null) {
-                                          setState(() {
-                                            selectedDomain = newValue;
-                                            _updateEmail();
-                                          });
-                                        }
-                                      },
-                                      items: emailDomains
-                                          .map<DropdownMenuItem<String>>(
-                                        (String domain) {
-                                          return DropdownMenuItem<String>(
-                                            value: domain,
-                                            child: Text(
-                                              "@$domain",
-                                              style: GoogleFonts.getFont(
-                                                'Inter',
-                                                color: ColorTheme
-                                                    .color.textWhiteColor,
-                                                fontWeight: FontWeight.w400,
-                                                fontSize: 16,
-                                              ),
-                                            ),
-                                          );
-                                        },
-                                      ).toList(),
-                                    ),
-                                  ),
-                                  keyboardType: TextInputType.emailAddress,
-                                  autofillHints: [AutofillHints.email],
-                                  onChanged: (value) => _updateEmail(),
-                                ),
-                                sizedBoxH15(),
-                                textFormField(
-                                  _passwordController,
-                                  'Password',
-                                  "Enter your password...",
-                                  (value) {
-                                    if (value == null || value.isEmpty) {
-                                      return 'Please enter a password';
-                                    }
-                                    if (value.length < 8) {
-                                      return 'Password must be at least 8 characters long';
-                                    }
-                                    RegExp regex = RegExp(
-                                        r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$');
-                                    if (!regex.hasMatch(value)) {
-                                      return 'Password must contain at least one uppercase letter, one number, and one special character';
-                                    }
-                                    return null;
-                                  },
-                                  suffixIcon: IconButton(
-                                    icon: Icon(
-                                      _obscureText
-                                          ? Icons.visibility_off
-                                          : Icons.visibility,
-                                    ),
-                                    onPressed: () {
-                                      setState(() {
-                                        _obscureText = !_obscureText;
-                                      });
-                                    },
-                                  ),
-                                  obscureText: _obscureText,
-                                  autovalidateMode:
-                                      AutovalidateMode.onUserInteraction,
-                                ),
-                                sizedBoxH15(),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    textButton(
-                                      'Forget Password?',
-                                      () => materialNamedRouteNavigator(
-                                          context, '/forgetPage'),
-                                    ),
-                                    elevatedButton(
-                                      'Login',
-                                      () => materialNamedRouteNavigator(
-                                          context, '/dashboardPage'),
-                                    ),
-                                  ],
-                                ),
-                                sizedBoxH15(),
-                                Opacity(
-                                  opacity: 0.8,
-                                  child: InkWell(
-                                    onTap: () {},
-                                    child: Container(
-                                      width:
-                                          isLargeScreen ? 400 : double.infinity,
-                                      height: 44,
-                                      decoration: BoxDecoration(
-                                        color: Colors.black.withOpacity(0.7),
-                                        borderRadius: BorderRadius.circular(8),
-                                      ),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          googleInterText(
-                                            'Don\'t have an account?',
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.w400,
-                                          ),
-                                          Padding(
-                                            padding: EdgeInsets.only(left: 5),
-                                            child: InkWell(
-                                              onTap: () =>
-                                                  materialNamedRouteNavigator(
-                                                      context, '/registerPage'),
-                                              child: googleInterText(
-                                                'Create',
-                                                color: ColorTheme.color
-                                                    .buttonBackgroundColor,
-                                                fontSize: 14,
-                                                fontWeight: FontWeight.w400,
-                                              ),
-                                            ),
-                                          ),
-                                          Icon(
-                                            Icons.arrow_forward_rounded,
-                                            color: ColorTheme
-                                                .color.buttonBackgroundColor,
-                                            size: 24,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
               ),
             ],
           );
